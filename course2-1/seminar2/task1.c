6bcb556f6eabb12c2d0810b7fc5af750a2e31d7b
Ivan Shapovalov <intelfx@intelfx.name>
2016-09-14 18:14:16 +0300

    course2-1: seminar2: update solutions for new formulations of the tasks.

--- course2-1/seminar2/task1.c
+++ course2-1/seminar2/task1.c
@@ -1,21 +1,69 @@
-#include <unistd.h>
 #include <stdio.h>
-#include <alloca.h>
-#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <errno.h>
+
+long parse_integer(const char *arg)
+{
+	char *endptr = NULL;
+	errno = 0;
+	long value = strtol(arg, &endptr, 10);
+
+	if (*arg == '\0' || *endptr != '\0') {
+		fprintf (stderr, "Argument is not a valid decimal number: '%s'\n", arg);
+		exit(1);
+	}
+
+	if (errno) {
+		fprintf (stderr, "Argument parse error: '%s': %m\n", arg);
+		exit(1);
+	}
+
+	return value;
+}
 
 int main(int argc, char **argv)
 {
-	if (argc < 2) {
-		fprintf(stderr, "This program expects at least one argument.\n");
+	if (argc != 2) {
+		fprintf(stderr, "This program expects one argument.\n");
 		return 1;
 	}
 
-	int arg_cnt = argc - 1;
-	char **exec_argv = alloca(sizeof(char*) * (arg_cnt + 1));
-	memcpy(exec_argv, argv + 1, sizeof(char*) * arg_cnt);
-	exec_argv[arg_cnt] = NULL;
+	int fork_failed = 0;
+	long children_count = parse_integer(argv[1]);
 
-	execvp(exec_argv[0], exec_argv);
-	fprintf(stderr, "Failed to execvp(): %m\n");
-	return 1;
+	for (long i = 0; i < children_count; ++i) {
+		long child = fork();
+
+		if (child < 0) {
+			/* parent, fork() failed */
+			fprintf(stderr, "Failed to fork() child #%zu: %m\n", i + 1);
+			fork_failed = 1;
+		} else if (child == 0) {
+			/* child */
+			printf("child #%zu pid %ld ppid %ld\n", i + 1, (long)getpid(), (long)getppid());
+			return 0;
+		}
+
+		/* parent, fork() succeeded */
+#ifdef WAIT
+		int child_status;
+		long r = waitpid(child, &child_status, 0);
+		if (r < 1) {
+			fprintf(stderr, "Failed to wait() for pid %ld: %m\n", (long)child);
+			return 1; /* we cannot guarantee ordering anymore */
+		} else if (r != child) {
+			fprintf(stderr, "Unexpected return from wait() for pid %ld: %ld\n", (long)child, (long)r);
+			return 1; /* we cannot guarantee ordering anymore */
+		} else {
+			if(!WIFEXITED(child_status) || (WEXITSTATUS(child_status) != 0)) {
+				fprintf(stderr, "child #%zu (pid %ld) failed somewhy\n", i + 1, (long)child);
+			}
+			/* otherwise it's ok */
+		}
+#endif
+	}
+
+	return fork_failed ? 1 : 0;
 }
--- course2-1/seminar2/task2.c
+++ course2-1/seminar2/task2.c
@@ -1,53 +1,22 @@
-#include <stdio.h>
 #include <unistd.h>
-#include <sys/wait.h>
-#include <stdlib.h>
-#include <errno.h>
-
-long parse_integer(const char *arg)
-{
-	char *endptr = NULL;
-	errno = 0;
-	long value = strtol(arg, &endptr, 10);
-
-	if (*arg == '\0' || *endptr != '\0') {
-		fprintf (stderr, "Argument is not a valid decimal number: '%s'\n", arg);
-		exit(1);
-	}
-
-	if (errno) {
-		fprintf (stderr, "Argument parse error: '%s': %m\n", arg);
-		exit(1);
-	}
-
-	return value;
-}
+#include <stdio.h>
+#include <alloca.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {
-	if (argc != 2) {
-		fprintf(stderr, "This program expects one argument.\n");
+	if (argc < 2) {
+		fprintf(stderr, "This program expects at least one argument.\n");
 		return 1;
 	}
 
-	int fork_failed = 0;
-	long children_count = parse_integer(argv[1]);
+	int arg_cnt = argc - 1;
 
-	for (long i = 0; i < children_count; ++i) {
-		long child = fork();
+	char *exec_argv[arg_cnt + 1];
+	memcpy(exec_argv, argv + 1, sizeof(char*) * arg_cnt);
+	exec_argv[arg_cnt] = NULL;
 
-		if (child < 0) {
-			/* parent, fork() failed */
-			fprintf(stderr, "Failed to fork() child #%zu: %m\n", i + 1);
-			fork_failed = 1;
-		} else if (child == 0) {
-			/* child */
-			printf("child #%zu pid %ld ppid %ld\n", i + 1, (long)getpid(), (long)getppid());
-			return 0;
-		}
-
-		/* parent, fork() succeeded */
-	}
-
-	return fork_failed ? 1 : 0;
+	execvp(exec_argv[0], exec_argv);
+	fprintf(stderr, "Failed to execvp(): %m\n");
+	return 1;
 }
